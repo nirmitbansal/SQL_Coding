0204b9c86627bc334d3e4233bd575b1c4197c745
NIRMIT BANSAL <bansalnirmit3@gmail.com>
2026-02-14 11:50:10 +0530

    Time: 328 ms (58.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,9 @@
+SELECT E.employee_id
+FROM Employees AS E
+LEFT JOIN Employees AS M
+  ON E.manager_id = M.employee_id
+WHERE
+  E.salary < 30000
+  AND E.manager_id IS NOT NULL
+  AND M.employee_id IS NULL
+ORDER BY E.employee_id;
